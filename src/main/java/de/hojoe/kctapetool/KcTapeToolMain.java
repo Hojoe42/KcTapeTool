c46bc63f2773cf4b33b22750ba123ebc73a15d00
Holger Jödicke <gitpub@hojoe.de>
2024-07-29 12:14:27 +0200

    Beispiel Argumente aus der Main heruasgenommen

--- src/main/java/de/hojoe/kctapetool/KcTapeToolMain.java
+++ src/main/java/de/hojoe/kctapetool/KcTapeToolMain.java
@@ -15,15 +15,6 @@ public class KcTapeToolMain
    */
   public static void main(String[] args)
   {
-//    args = new String[] { "--help" };
-//    args = new String[] { "-V" };
-//    args = new String[] { "--version" };
-//    args = new String[] { "-s", "Eingang" };
-//    args = new String[] { "-s", "test1.wav", "--directory", "d:/tmp/kc" };
-//    args = new String[] { "-l" };
-//    args = new String[] { "--list" };
-//    args = new String[] { "-s", "Eingang", "--wait", "30", "-d", "d:/tmp/kc/out.bla"};
-    args = new String[] { "-s", "d:/tmp/kc/CAOS_E.KCC"};
       int rc = new CommandLine(new KcTapeToolCommand()).execute(args);
       System.exit(rc);
   }
